eacbaa431cdf552199a98d9691deee975318e7ae
Priscila <prianjss@gmail.com>
2024-10-12 15:02:51 -0300

    feat: tela empresa

---
+++ omni-solve-frontend/src/app/admin/telaempresa/page.js
@@ -0,0 +1,15 @@
+//import Header from '../components/Header';
+//import styles from '../styles/Home.module.css';
+
+const Home = () => {
+  return (
+    <div>
+      <main>
+        <h1>Bem-vindo ao Omni Solve!</h1>
+        <p>Esta é a página inicial do seu projeto.</p>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
